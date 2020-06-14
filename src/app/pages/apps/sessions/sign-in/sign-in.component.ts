f25b20055d7599c83fa8b7846fceab84ff4a3647
okeleye kabiru <54416255+okeleyekabiru@users.noreply.github.com>
2020-06-14 17:14:59 +0100

    Add dashboard

--- src/app/pages/apps/sessions/sign-in/sign-in.component.ts
+++ src/app/pages/apps/sessions/sign-in/sign-in.component.ts
@@ -1,12 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { BasePageComponent } from '../../../..//pages/base-page';
+import { IAppState } from '../../../../interfaces/app-state';
+import { Store } from '@ngrx/store';
+import { HttpService } from '../../../../services/http/http.service';
 
 @Component({
   selector: 'page-sign-in',
   templateUrl: './sign-in.component.html',
   styleUrls: ['./sign-in.component.scss']
 })
-export class PageSignInComponent implements OnInit {
-  constructor() { }
+export class PageSignInComponent extends BasePageComponent implements OnInit {
+  constructor(
+    store: Store<IAppState>,
+    httpSv: HttpService,
+   
+  ) {
+    super(store, httpSv);
+  }
 
-  ngOnInit() { }
+  ngOnInit() { 
+    console.log("i loaded")
+    this.setLoaded()
+  }
 }
